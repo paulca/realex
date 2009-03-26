d6703d2e72cb7acbb855c324f1ef90acddba6d32
Paul Campbell <paul@rslw.com>
2009-03-26 18:01:39 +0000

    remove very blatant Twitter gem rips!

--- lib/real_ex.rb
+++ lib/real_ex.rb
@@ -16,13 +16,6 @@ require 'real_ex/response'
  
 module RealEx
   class UnknownError < StandardError; end
-  class CantConnect < StandardError; end
-  class BadResponse < StandardError; end
-  class UnknownTimeline < ArgumentError; end
-  class RateExceeded < StandardError; end
-  class CantFindUsers < ArgumentError; end
-  class AlreadyFollowing < StandardError; end
-  class CantFollowUser < StandardError; end
  
   SourceName = 'twittergem'
 end
