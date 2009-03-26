92064b85aee58620eae25a178122f3e43db25a72
Paul Campbell <paul@rslw.com>
2009-03-26 17:53:55 +0000

    prime the response

--- lib/real_ex/response.rb
+++ lib/real_ex/response.rb
@@ -1,9 +1,13 @@
 module RealEx
   class Response
+    class CardIssuer
+      include Initializer
+      attributes :bank, :country, :countrycode, :region
+    end
     
     include Initializer
     
-    attributes :timestamp, :result, :message, :orderid
+    attributes :timestamp, :result, :message, :orderid, :merchantid, :account, :cvnresult, :avspostcoderesponse, :pasref, :timetaken, :authtimetaken, :batchid, :avsaddressresponse, :cardissuer
     
     def self.new_from_xml(xml)
       parsed_xml = xml.kind_of?(String) ? Hpricot.XML(xml) : xml
@@ -12,6 +16,22 @@ module RealEx
       r.result = (parsed_xml).at('result').inner_html if (parsed_xml).at('result')
       r.message = (parsed_xml).at('message').inner_html if (parsed_xml).at('message')
       r.orderid = (parsed_xml).at('orderid').inner_html if (parsed_xml).at('orderid')
+      r.merchantid = (parsed_xml).at('merchantid').inner_html if (parsed_xml).at('merchantid')
+      r.account = (parsed_xml).at('account').inner_html if (parsed_xml).at('account')
+      r.cvnresult = (parsed_xml).at('cvnresult').inner_html if (parsed_xml).at('cvnresult')
+      r.avspostcoderesponse = (parsed_xml).at('avspostcoderesponse').inner_html if (parsed_xml).at('avspostcoderesponse')
+      r.avsaddressresponse = (parsed_xml).at('avsaddressresponse').inner_html if (parsed_xml).at('avsaddressresponse')
+      r.batchid = (parsed_xml).at('batchid').inner_html if (parsed_xml).at('batchid')
+      r.pasref = (parsed_xml).at('pasref').inner_html if (parsed_xml).at('pasref')
+      r.timetaken = (parsed_xml).at('timetaken').inner_html if (parsed_xml).at('timetaken')
+      r.authtimetaken = (parsed_xml).at('authtimetaken').inner_html if (parsed_xml).at('authtimetaken')
+      if cardissuer = (parsed_xml).at('cardissuer')
+        r.cardissuer = CardIssuer.new()
+        r.cardissuer.bank = cardissuer.at('bank').inner_html
+        r.cardissuer.country = cardissuer.at('country').inner_html
+        r.cardissuer.countrycode = cardissuer.at('countrycode').inner_html
+        r.cardissuer.region = cardissuer.at('region').inner_html
+      end
       r
     end
   end
--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -22,3 +22,49 @@ describe "RealEx::Response" do
     @response.orderid.should == '1234'
   end
 end
+
+describe "RealEx::Response" do
+  before do
+    @response = RealEx::Response.new_from_xml(%Q[<?xml version="1.0" encoding="UTF-8" ?>
+<response timestamp="20090326171616">
+<merchantid>test</merchantid>
+<account>Realex</account>
+<orderid>12345</orderid>
+<authcode>171616</authcode>
+<result>00</result>
+<cvnresult>U</cvnresult>
+<avspostcoderesponse>U</avspostcoderesponse>
+<avsaddressresponse>U</avsaddressresponse>
+<batchid>136</batchid>
+<message>[ test system ] Authorised 171616</message>
+<pasref>12380877763476</pasref>
+<timetaken>1</timetaken>
+<authtimetaken>0</authtimetaken>
+<cardissuer>
+<bank>AIB BANK</bank>
+<country>IRELAND</country>
+<countrycode>IE</countrycode>
+<region>EUR</region>
+</cardissuer>])
+  end
+  
+  it "should build the object" do
+    @response.timestamp.should == '20090326171616'
+    @response.merchantid.should == 'test'
+    @response.account.should == 'Realex'
+    @response.orderid.should == '12345'
+    @response.cardissuer.bank.should == 'AIB BANK'
+    @response.cvnresult.should == 'U'
+    @response.cvnresult.should == 'U'
+    @response.avspostcoderesponse.should == 'U'
+    @response.avsaddressresponse.should == 'U'
+    @response.batchid.should == '136'
+    @response.message.should == '[ test system ] Authorised 171616'
+    @response.pasref.should == '12380877763476'
+    @response.timetaken.should == '1'
+    @response.authtimetaken.should == '0'
+    @response.cardissuer.country.should == 'IRELAND'
+    @response.cardissuer.countrycode.should == 'IE'
+    @response.cardissuer.region.should == 'EUR'
+  end
+end
