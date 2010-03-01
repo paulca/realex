78442a9ac8452c0bf8defd605f8094d97d8330e5
Rob Holland <rob@the-it-refinery.co.uk>
2010-03-01 13:21:30 +0000

    Default value fixups.

--- lib/real_ex/address.rb
+++ lib/real_ex/address.rb
@@ -4,10 +4,15 @@ module RealEx
     attributes :street, :city, :county
     attributes :post_code, :country, :country_code, :phone_numbers, :email
     
+    def initialize(*args)
+      super
+      @phone_numbers ||= {}
+    end
+    
     [1,2,3].each do |line|
       class_eval do
         define_method("line#{line}") do
-          street.split("\n")[line - 1]
+          street.to_s.split("\n")[line - 1]
         end
       end
     end
