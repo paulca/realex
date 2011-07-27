f74776a992947e5b8c33891954ea88973bc4c686
Paul Springett <paul@simpleweb.co.uk>
2011-07-27 17:58:29 +0100

    Re-added missing Realex call from recurring transaction class

--- lib/real_ex/recurring.rb
+++ lib/real_ex/recurring.rb
@@ -3,7 +3,7 @@ module RealEx
     
     class Transaction < RealEx::Transaction
       def authorize!
-        RealEx::Response.new_from_xml(real_vault_uri, to_xml))
+        RealEx::Response.new_from_xml(RealEx::Client.call(real_vault_uri, to_xml))
       end
     end
     
